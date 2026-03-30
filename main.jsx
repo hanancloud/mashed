2fb56527ea8c3a0d48622d7be78e0099bc7ffb5f
hanancloud <alhananrealestate@gmail.com>
2026-03-30 10:45:36 +0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
@@ -8,10 +7,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-      <Analytics />
-      <SpeedInsights />
-    </BrowserRouter>
+    <App />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
